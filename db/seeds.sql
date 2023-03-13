d7ba167651d1181b8f17883dc21a7d06c0a0b469
camcrow <CameronCroteau2020@gmail.com>
2023-03-13 18:00:26 -0500

    changes for main

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO department (name)
+VALUES ("Manager"), ("Shift Lead"), ("Cashier"), ("Cook"), ("Dishwasher");
+
+INSERT INTO role (title, salary, department_id)
+VALUE ("Manager", 50000.00, 1), ("Shift Lead", 35000.00, 2), ("Cashier", 25000.00, 3), ("Cook", 24000.00, 4), ("Dishwasher", 20000.00, 5);
+
+INSERT INTO employee (last_name, first_name, role_id,)
+VALUE ("Johnson", "John", 1, 3), ("Smith", "Mark", 1, 1), ("Brown", "David", 3, 2), ("Stevens", "Sam", 5, 2), ("Williams", "Henry", 5, 2);
